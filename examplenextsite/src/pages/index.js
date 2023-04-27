7b82178c95c66ad1cb25f53aaa55e9d3fce2becd
BuildTools <unconfigured@null.spigotmc.org>
2023-04-26 19:17:48 -0500

    y

--- examplenextsite/src/pages/index.js
+++ examplenextsite/src/pages/index.js
@@ -43,7 +43,6 @@ export default function Home() {
   </a>
   </div>
 </div>
-<div id="nottop">
 <div
         id="images"
         style={{
@@ -68,7 +67,6 @@ export default function Home() {
         <li className="coffeelisttwelve">12oz.<ul className="subtwelvehot">(Hot) ...2.95</ul><ul className="subtwelvecold">(Iced)** ...3.35</ul></li><li className="coffeelistsix">16oz. <ul className="subsixhot">(Hot) ...3.95</ul><ul className="subsixcold">(Iced)** ...4.25</ul></li>
         <h2 id="milknote">**Iced coffee can be served<br></br> with condensed milk. Yum!</h2>
        </div>
-     </div>
      </div>
     </>
   );
