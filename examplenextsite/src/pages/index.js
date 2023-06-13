3e81dd3262d00c308203ea983aed86f2171e1bd5
KadeStanford <70962025+KadeStanford@users.noreply.github.com>
2023-06-13 13:36:25 -0500

    Updated prices i think

--- examplenextsite/src/pages/index.js
+++ examplenextsite/src/pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
         <li className=" coffeelisttwelve"><div id="hot">Hot</div><ul className="coffeelist subtwelvehot">(12oz.)<br></br>  ...2.95</ul>
         <ul className="subtwelvecold">(16oz) <br></br> ...3.95</ul></li>
         <li className="coffeelistsix"><div id="iced">Iced**</div><ul className="subsixhot">(12oz.)<br></br> ...3.45</ul>
-        <ul className="subsixcold">(16oz.) <br></br> ...4.35</ul>
+        <ul className="subsixcold">(16oz.) <br></br> ...4.45</ul>
         </li>
         <div id="milknote"><div id="stroop"><div id="coffeeflavors">Coffee Flavors:<br></br>Mocha, Vanilla (regular or sugar-free), Caramel, White Chocolate</div></div><br></br><div id="icednote">**Iced coffee can be served with condensed milk. Yum!</div></div>
        </div>
@@ -143,13 +143,13 @@ export default function Home() {
         <li id="buttered">
         <div id="croissantflavor">Buttered</div><div id="cp1">...2.95</div></li>  
         <li id="ham">
-        <div id="croissantflavor">Ham and Swiss</div><div id="cp2">...5.85</div></li>
+        <div id="croissantflavor">Ham and Swiss</div><div id="cp2">...5.95</div></li>
         <li id="spinach">
-        <div id="croissantflavor">Spinach and Feta</div><div id="cp5">...5.75</div></li>
+        <div id="croissantflavor">Spinach and Feta</div><div id="cp5">...5.95</div></li>
         <li id="almond">
         <div id="croissantflavor">Almond</div><div id="cp3">...3.95</div></li>
         <li id="chocolate">
-        <div id="croissantflavor">Chocolate</div><div id="cp4">...3.25</div></li>
+        <div id="croissantflavor">Chocolate</div><div id="cp4">...3.75</div></li>
        </div>
        </div> 
     </div>
@@ -160,7 +160,7 @@ export default function Home() {
     <div className="menu-content">
     <div id="scones">Scones</div>
       <div id="menuscone">
-      <li id="sconeprice"><div id="blueberry">Blueberry</div><div id="bp">...3.75</div></li>
+      <li id="sconeprice"><div id="blueberry">Blueberry</div><div id="bp">...3.89</div></li>
       </div>
     </div>
     </div>  
@@ -169,7 +169,7 @@ export default function Home() {
       <img id="brownimage" src="/images/brown.png" alt="Brown"></img> 
       <div className="menu-content">
       <div id="brownsugarcakeheader">Brown Sugar Walnut<br></br>Coffee Cake</div>
-      <div id="menubrownsugarcake"> <li id="cakeprice"><div id="slice">Per Slice</div><div id="sliceprice">...3.35</div></li> </div>
+      <div id="menubrownsugarcake"> <li id="cakeprice"><div id="slice">Per Slice</div><div id="sliceprice">...3.59</div></li> </div>
       
       </div>
       </div>
@@ -193,12 +193,32 @@ export default function Home() {
           </div>
           </div>
 
+ <div className="image-container">
+          <img  id="raspberrydanishimage" src="/images/muffin.jgp" alt="Muffin"></img>
+          <div className="menu-content">
+            
+      <div id="raspberrydanishheader">Morning Harvest Muffin</div>
+      <div id="menuraspberrydanish"> <li id="raspberrydanishprice"><div id="sliceraspberry">Per Muffin</div><div id="slicepriceraspberry">...3.39</div></li> </div>
+
+          </div>
+          </div>
+
+ <div className="image-container">
+          <img  id="raspberrydanishimage" src="/images/cookie.png" alt="cookie"></img>
+          <div className="menu-content">
+            
+      <div id="raspberrydanishheader">Extra Lg. Choc Chunk Cookie</div>
+      <div id="menuraspberrydanish"> <li id="raspberrydanishprice"><div id="sliceraspberry">Per Cookie</div><div id="slicepriceraspberry">...3.69</div></li> </div>
+
+          </div>
+          </div>
+
           <div className="image-container">
             <img id="biscuitimage" src="/images/biscuit.jpg" alt="Biscuit"></img>
             <div className="menu-content">
               
       <div id="biscuitheader">Biscuits</div>
-      <div id="menubiscuit"> <li id="biscuitprice"><div id="biscuitprice1">Large Buttermilk Biscuit w/ Butter</div><div id="biscuitprice2">...2.59</div></li> <li id="fixings">Add Preserves (Strawberry or Blackberry) ...0.59<br></br>Add Deer Sausage Patty ...1.99<br></br>Add Egg Bite* ...3.59</li></div>
+      <div id="menubiscuit"> <li id="biscuitprice"><div id="biscuitprice1">Large Buttermilk Biscuit w/ Butter</div><div id="biscuitprice2">...2.59</div></li> <li id="fixings">Add Preserves (Strawberry or Blackberry) ...0.59<br></br>Add Deer Sausage Patty ...1.99<br></br>Add Egg Bite* ...3.69</li></div>
 
             </div>
             </div>
@@ -207,7 +227,7 @@ export default function Home() {
               <img id="bagelimage" src="/images/bagel.png" alt="Bagel"></img>
               <div className="menu-content">
       <div id="bagelheader">Bagels</div>
-      <div id="menubagel"> <li id="bagelprice"><div id="bagelprice1">Plain Bagel w/ Butter<br></br></div><div id="bagelprice2">...2.69</div></li> <li id="fixings2">(Substitute Cinnamon Butter  ...0.30) <br></br>Add Preserves (Strawberry or Blackberry) ...0.59<br></br>Add Deer Sausage Patty ...1.99<br></br>Add Cream Cheese ...0.99<br></br>Add Egg Bite* ...3.59</li></div>
+      <div id="menubagel"> <li id="bagelprice"><div id="bagelprice1">Plain Bagel w/ Butter<br></br></div><div id="bagelprice2">...2.69</div></li> <li id="fixings2">(Substitute Cinnamon Butter  ...0.30) <br></br>Add Preserves (Strawberry or Blackberry) ...0.59<br></br>Add Deer Sausage Patty ...1.99<br></br>Add Cream Cheese ...0.99<br></br>Add Egg Bite* ...3.69</li></div>
 
               </div>
               </div>
@@ -216,7 +236,7 @@ export default function Home() {
                 <img id="eggimage" src="/images/egg.png" alt="Egg"></img>
                 <div className="menu-content">
       <div id="eggheader">Egg Bites</div>
-      <div id="menuegg"> <li id="eggprice"><div id="eggprice1">Egg Bite* (2 per order)</div><div id="eggprice2">...5.99</div></li> <li id="eggfixings">*Options: 1. Ham and Cheddar Chili Pepper,
+      <div id="menuegg"> <li id="eggprice"><div id="eggprice1">Egg Bite* (2 per order)</div><div id="eggprice2">...6.59</div></li> <li id="eggfixings">*Options: 1. Ham and Cheddar Chili Pepper,
 <br></br>2. Prosciutto gruyere, 3. Spinach Feta</li></div>
 
           </div>
