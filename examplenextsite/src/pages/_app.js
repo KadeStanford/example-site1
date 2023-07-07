ad0780919a65411baf63a0014988d02551290cc1
kade <kade20413@gmail.com>
2023-07-07 16:24:10 -0500

    added scroll stuff

--- examplenextsite/src/pages/_app.js
+++ examplenextsite/src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Scrollbar } from "react-scrollbars-custom";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
